7d0090b75293da1cdfd38046ffe086f6ac3d87bd
Alexey Sidorov <alexey.sidorov.dev@mail.ru>
2021-12-13 00:45:31 +0300

    add workflow, add more webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,64 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const path = require("path");
+const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = {
   entry: path.resolve(__dirname, "src/index.js"),
+  devtool: NODE_ENV === "production" ? "nosources-source-map" : "eval-source-map",
+  mode: NODE_ENV === "production" ? "production" : "development",
+
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
       {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "./images/[contenthash][ext]",
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
     ],
   },
+
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
     clean: true,
+    environment: {
+      arrowFunction: false,
+    },
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src/index.html"),
     }),
     new MiniCssExtractPlugin(),
   ],
+
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
+  },
+
   devServer: {
     compress: true,
     port: 9000,
@@ -31,3 +67,8 @@ module.exports = {
     },
   },
 };
+
+if (NODE_ENV === "development") {
+  module.exports.devtool = "eval-source-map";
+  module.exports.mode = "development";
+}
