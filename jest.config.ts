569ff6189c327f10f731db8ea53aab8bba651ef2
Alexey Sidorov <alexey.sidorov.dev@mail.ru>
2023-03-17 23:56:44 +0300

    fix jest coverage

--- jest.config.ts
+++ jest.config.ts
@@ -11,14 +11,14 @@ const config: Config = {
   // collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "!./**/index.{js,ts}",
-    "!./**/helpers/**",
-    "!./**/interfaces/**",
-    "!./**/types/**",
-    "!./**/*.d.ts",
-  ],
+  // collectCoverageFrom: [
+  //   "./src/**/*.{js,ts,jsx,tsx}",
+  //   "!./**/index.{js,ts}",
+  //   "!./**/helpers/**",
+  //   "!./**/interfaces/**",
+  //   "!./**/types/**",
+  //   "!./**/*.d.ts",
+  // ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
